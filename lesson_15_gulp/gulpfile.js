fe3fb016bad22b89556780e1b9da3b182857aaa9
Sergey Chuprina <sergey.chuprina@hyuna.bb>
2018-10-23 19:25:16 +0300

    gulp: gulpfile modified

--- lesson_15_gulp/gulpfile.js
+++ lesson_15_gulp/gulpfile.js
@@ -1,26 +1,34 @@
 'use strict';
 
 const   gulp = require('gulp'),
-       autoprefixer = require('gulp-autoprefixer'),
-       sass = require('gulp-sass'),
-       browserSync = require('browser-sync').create();
+autoprefixer = require('gulp-autoprefixer'),
+notify = require('gulp-notify'),
+sass = require('gulp-sass'),
+clean = require('gulp-clean'),
+browserSync = require('browser-sync').create();
 
 
 gulp.task('sass', () => {
-   return gulp.src('src/scss/**/*.scss')
-   .pipe(sass().on('error', sass.logError))
-   .pipe(autoprefixer({
-       browsers: ['last 2 versions'],
-       cascade: false
-   }))
-   .pipe(gulp.dest('app/css'))
-   .pipe(browserSync.stream());
+ return gulp.src('src/scss/**/*.scss')
+ .pipe(sass().on('error', notify.onError("SASS-Error: <%= error.message %>")))
+ .pipe(autoprefixer({
+   browsers: ['last 2 versions'],
+   cascade: false
+ }))
+ .pipe(gulp.dest('app/css'))
+ .pipe(browserSync.stream());
 });
 
 gulp.task('html', () => {
-   return gulp.src('src/index.html')
-   .pipe(gulp.dest('app/'))
-   .pipe(browserSync.stream());
+ return gulp.src('src/index.html')
+ .pipe(gulp.dest('app/'))
+ .pipe(browserSync.stream());
+});
+
+gulp.task('img', () => {
+ return gulp.src('src/img/**/*.*')
+ .pipe(gulp.dest('app/img'))
+ .pipe(browserSync.stream());
 });
 
 gulp.task('watch', () => {
@@ -29,13 +37,24 @@ gulp.task('watch', () => {
 });
 
 gulp.task('connect', function() {
-   browserSync.init({
-       server: {
-           baseDir: "app",
-           open: true
-       }
-   });
+ browserSync.init({
+   server: {
+     baseDir: "app",
+     open: true
+   }
+ });
 });
 
+gulp.task('clean', function () {
+  return gulp.src('app')
+  .pipe(clean({
+    force: true,
+    read: false
+  }));
+});
 
-gulp.task('default', ['watch', 'html', 'sass', 'connect']);
+gulp.task('developing', ['watch', 'html', 'img', 'sass', 'connect']);
+
+gulp.task('default', ['clean'], () => {
+  gulp.start('developing');
+});
