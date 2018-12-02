83965c43474b435c957e408a2aafb0e31aecb6e4
Sergey <davaynamore@gmail.com>
2018-12-02 10:43:00 +0200

    bootstrap gulp creato template added

---
+++ lesson_18_bootstrap/src/js/assets/imgs.js
@@ -0,0 +1,33 @@
+import logoImg from '../../img/logo.png';
+import aboutImgBg from '../../img/about-bg.jpg';
+import angles from '../../img/angles.svg';
+import galImg1 from '../../img/gal-img-1.jpg';
+import galImg2 from '../../img/gal-img-2.jpg';
+import galImg3 from '../../img/gal-img-3.jpg';
+import galImg4 from '../../img/gal-img-4.jpg';
+import galImg5 from '../../img/gal-img-5.jpg';
+import galImg6 from '../../img/gal-img-6.jpg';
+import headerImgBg from '../../img/header-bg.jpg';
+import hipsterLogoImg from '../../img/logo-hipster.png';
+import millerLogoImg from '../../img/logo-miller.png';
+import man1Img from '../../img/man1.jpg';
+import man2Img from '../../img/man2.jpg';
+import man3Img from '../../img/man3.jpg';
+
+export {
+	logoImg,
+	aboutImgBg,
+	angles,
+	galImg1,
+	galImg2,
+	galImg3,
+	galImg4,
+	galImg5,
+	galImg6,
+	headerImgBg,
+	hipsterLogoImg,
+	millerLogoImg,
+	man1Img,
+	man2Img,
+	man3Img
+}
