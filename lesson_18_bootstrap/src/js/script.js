99fec2c08ad4799d725455f36789f5f85439f094
Sergey <davaynamore@gmail.com>
2019-01-12 14:33:52 +0200

    floating tempate block

--- lesson_18_bootstrap/src/js/script.js
+++ lesson_18_bootstrap/src/js/script.js
@@ -41,6 +41,7 @@
 				userData[el.name] = el.value;
 				el.value = '';
 			})
+			console.log(userData);
 			return userData;
 		};
 
@@ -112,7 +113,7 @@
 
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+const keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
 function preventDefault(e) {
 	e = e || window.event;
